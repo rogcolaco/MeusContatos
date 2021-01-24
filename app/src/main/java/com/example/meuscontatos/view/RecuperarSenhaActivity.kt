36f88ab9efcd9edc55b2c7ab6a837bd24d0cdaa0
Rogerio Colaço <rcs@ufscar.br>
2021-01-24 17:33:18 -0300

    - Recuperar Senha Activity.

--- app/src/main/java/com/example/meuscontatos/view/RecuperarSenhaActivity.kt
+++ app/src/main/java/com/example/meuscontatos/view/RecuperarSenhaActivity.kt
@@ -3,6 +3,8 @@ package com.example.meuscontatos.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.example.meuscontatos.controller.AutenticadorFirebase
 import com.example.meuscontatos.databinding.ActivityRecuperarSenhaBinding
 
 class RecuperarSenhaActivity : AppCompatActivity() {
@@ -16,5 +18,14 @@ class RecuperarSenhaActivity : AppCompatActivity() {
         setContentView(activityRecuperarSenhaBinding.root)
     }
 
-    fun onClick(view: View) {}
+    fun onClick(view: View) {
+        if(view == activityRecuperarSenhaBinding.enviarEmailBt){
+            val email = activityRecuperarSenhaBinding.emailRecuperacaoSenhaEt.text.toString()
+            if(email.isNotBlank() && email.isNotEmpty()){
+                AutenticadorFirebase.firebaseAuth.sendPasswordResetEmail(email)
+                Toast.makeText(this,"E-mail de recuperacao enviado para o $email.", Toast.LENGTH_SHORT).show()
+            }
+            finish()
+        }
+    }
 }
