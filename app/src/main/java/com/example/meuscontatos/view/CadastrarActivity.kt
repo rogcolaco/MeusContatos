94a9a26934395aa89c8e7b85c75e297b142e5b93
Rogerio Colaço <rcs@ufscar.br>
2021-01-24 17:25:59 -0300

    - Cadastrar Activity - cadastro de login por e-mail.

--- app/src/main/java/com/example/meuscontatos/view/CadastrarActivity.kt
+++ app/src/main/java/com/example/meuscontatos/view/CadastrarActivity.kt
@@ -3,6 +3,8 @@ package com.example.meuscontatos.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.example.meuscontatos.controller.AutenticadorFirebase
 import com.example.meuscontatos.databinding.ActivityCadastrarBinding
 
 class CadastrarActivity : AppCompatActivity() {
@@ -18,5 +20,37 @@ class CadastrarActivity : AppCompatActivity() {
 
     fun onClick(view: View) {
 
+        if(view == activityCadastrarBinding.cadastrarBt) {
+            val email = activityCadastrarBinding.emailEt.text.toString()
+            val senha = activityCadastrarBinding.senhaEt.text.toString()
+            val repetirSenha = activityCadastrarBinding.repetirSenhaEt.text.toString()
+
+            if (saoValoresValidos(email, senha, repetirSenha)){
+                AutenticadorFirebase.firebaseAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(this,"Usuário $email criado com sucesso", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                    else {
+                        Toast.makeText(this,"Erro na criação do usuário", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else{
+                Toast.makeText(this,"Campos preenchidos incorretamente", Toast.LENGTH_SHORT).show()
+                activityCadastrarBinding.emailEt.requestFocus()
+
+            }
+        }
+    }
+
+    fun saoValoresValidos(email: String, senha: String, repetirSenha: String): Boolean{
+        return  if (email.isBlank() || email.isEmpty())
+            false
+        else if (senha.isBlank() || senha.isEmpty())
+            false
+        else if (repetirSenha.isBlank() || repetirSenha.isEmpty())
+            false
+        else senha.equals(repetirSenha)
     }
 }
